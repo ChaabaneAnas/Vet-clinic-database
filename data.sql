45ccba7b02195fce91efa6ee6ad862075f5c8a7a
Anas Chaabane <Anas.chaabane98@gmail.com>
2022-10-19 15:59:59 +0100

    insert data
    insert data

--- data.sql
+++ data.sql
@@ -1,5 +1,6 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+insert into animals ( name, date_of_birth, escape_attempts, neutered, weight_kg) values ('Agumon','feb 03, 2020', 0, true, 10.25);
+insert into animals ( name, date_of_birth, escape_attempts, neutered, weight_kg) values ('Gabumon','nov 15, 2018', 2, true, 8);
+insert into animals ( name, date_of_birth, escape_attempts, neutered, weight_kg) values ('Pikachu','Jan 7, 2021', 2, false, 15.04);
+insert into animals ( name, date_of_birth, escape_attempts, neutered, weight_kg) values ('Devimon','May 12, 2017', 5, true, 11);
