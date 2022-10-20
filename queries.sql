049770d9621cc02411283b704084b586c3a97700
Anas <anas.chaabane98@gmail.com>
2022-10-20 13:16:55 +0100

    analytic queries

--- queries.sql
+++ queries.sql
@@ -10,22 +10,36 @@ select * from animals where weight_kg >= 10.4 and weight_kg <= 17.3;
 /* Milestone (2)*/
 begin;
 update animals set species ='unspecified';
+select name, species from animals;
 rollback;
 
 begin;
 update animals set species = 'digimon' where name like '%mon';
-update aimals set species = 'pokemon' where species is null;
+update animals set species = 'pokemon' where species is null;
+select name, species from animals;
 commit;
+
 
 begin; 
 delete from animals;
+select name, date_of_birth, neutered, escape_attempts, species  from animals;
 rollback;
 
 begin;
-delete from animals where data_of_birth > 'jan 1, 2022';
-savepoint savepoint_1
+delete from animals where date_of_birth > 'jan 1, 2022';
+savepoint savepoint_1;
 update animals set weight_kg = weight_kg *-1;
 rollback to savepoint savepoint_1;
+select name, date_of_birth, neutered, escape_attempts, species  from animals;
+update set weight_kg = weight_kg *-1 where weight_kg < 0;
 commit;
+
+select count (*) from animals;
+select count (*) from animals where escape_attempts = 0;
+select avg(weight_kg) from animals;
+select neutered, sum(escape_attempts) from animals group by neutered;
+select species, min(weight_kg), max(weight_kg) from animals group by species;
+
+select species, avg(escape_attempts) from animals where date_of_birth between 'jan 1,1990' and 'jan 1, 2000' group by species ;
 
 
