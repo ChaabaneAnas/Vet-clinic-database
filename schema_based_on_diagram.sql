91f79712580d4856476cf5f4fda813d64c5dc7af
t-bello7 <bello4aus@gmail.com>
2022-10-26 18:24:29 +0100

    - create invoice_items table - create treatments table - create many-to-many table for treatments and medical records

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -15,8 +15,33 @@ task B
 - many to one relationship with invoices
 - many to one relationship with treatments
 
+CREATE TABLE invoice_items (
+id INT GENERATED ALWAYS AS IDENTITY,
+unit_price DECIMAL,
+quantity INT,
+total_price DECIMAL,
+invoice_id INT NOT NULL,
+treatment_id INT NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (invoice_id) REFERENCES invoices(id) ON DELETE CASCADE,
+FOREIGN KEY (treatment_id) REFERENCES treatments(id) ON DELETE CASCADE
+);
+
 # command for creating treatments table
-- many to many relationship with medical histories
-- on to many relationship with invoice_items
+
+CREATE TABLE treatments (
+id INT GENERATED ALWAYS AS IDENTITY,
+type VARCHAR,
+name VARCHAR,
+PRIMARY KEY (id),
+);
 
 # command for creating many to many relationship table between treatments and medical records
+CREATE TABLE treatments_medical_histories (
+id INT GENERATED ALWAYS AS IDENTITY,
+PRIMARY KEY (id),
+medical_histories_id INT NOT NULL,
+treatment_id INT NOT NULL,
+FOREIGN KEY(medical_histories_id) REFERENCES medical_histories(id) ON DELETE CASCADE,
+FOREIGN KEY(treatment_id) REFERENCES treatments(id) ON DELETE CASCADE,
+);
